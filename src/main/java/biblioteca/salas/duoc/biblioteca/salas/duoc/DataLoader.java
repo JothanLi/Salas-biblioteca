a57dcf225334c7432365c48d12ea780a82569bda
PAAPCA02LC207 <PAAPCA02LC207@PAAPCA02LC207.duoc.acad>
2025-06-11 22:22:41 -0400

    Desarrollo DataLoader, cambio en application.properties, integración de datafaker en carreras y salas

--- src/main/java/biblioteca/salas/duoc/biblioteca/salas/duoc/DataLoader.java
+++ src/main/java/biblioteca/salas/duoc/biblioteca/salas/duoc/DataLoader.java
@@ -1,6 +1,9 @@
 package biblioteca.salas.duoc.biblioteca.salas.duoc;
 
-import biblioteca.salas.duoc.biblioteca.salas.duoc.model.*;
+import biblioteca.salas.duoc.biblioteca.salas.duoc.model.Carrera;
+import biblioteca.salas.duoc.biblioteca.salas.duoc.model.Estudiante;
+import biblioteca.salas.duoc.biblioteca.salas.duoc.model.Sala;
+import biblioteca.salas.duoc.biblioteca.salas.duoc.model.TipoSala;
 import biblioteca.salas.duoc.biblioteca.salas.duoc.repository.*;
 import net.datafaker.Faker;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,45 +11,46 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-import java.util.Date;
 import java.util.List;
 import java.util.Random;
 
-@Profile("Jonathan")
+@Profile("dev")
 @Component
 public class DataLoader implements CommandLineRunner {
 
+    @Autowired
+    private TipoSalaRepository tipoSalaRepository;
     @Autowired
     private CarreraRepository carreraRepository;
     @Autowired
     private EstudianteRepository estudianteRepository;
     @Autowired
-    private ReservaRepository reservaRepository;
-    @Autowired
     private SalaRepository salaRepository;
     @Autowired
-    private TipoSalaRepository tipoSalaRepository;
+    private ReservaRepository reservaRepository;
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) throws Exception{
         Faker faker = new Faker();
         Random random = new Random();
 
-        for (int i = 0; i < 5; i++) {
+        //Creando tipos de sala
+        for(int i=0; i<3; i++){
             TipoSala tipoSala = new TipoSala();
             tipoSala.setNombre(faker.book().genre());
             tipoSalaRepository.save(tipoSala);
         }
 
-        for (int i = 0; i < 5; i++) {
+        for(int i=0; i<5; i++){
             Carrera carrera = new Carrera();
+            carrera.setCodigo(faker.code().asin());
             carrera.setNombre(faker.educator().course());
             carreraRepository.save(carrera);
         }
 
         List<Carrera> carreras = carreraRepository.findAll();
 
-        for (int i = 0; i < 10; i++) {
+        for(int i=0;i<50;i++){
             Estudiante estudiante = new Estudiante();
             estudiante.setRun(faker.idNumber().valid());
             estudiante.setNombre(faker.name().fullName());
@@ -62,24 +66,11 @@ public class DataLoader implements CommandLineRunner {
             sala.setNombre(faker.university().name());
             sala.setCapacidad(faker.number().numberBetween(10, 100));
             sala.setIdInstituto(faker.number().randomDigit());
-            sala.setTipoSala(tipoSalaRepository.findAll().get(random.nextInt(tipoSalaRepository.findAll().size())));
+            sala.setTipoSala(tipoSalaRepository.findAll().get(random.nextInt(3)));
             salaRepository.save(sala);
-        }
-
-        List<Estudiante> estudiantes = estudianteRepository.findAll();
-        List<Sala> salas = salaRepository.findAll();
-
-        for (int i = 0; i < 20; i++) {
-            Reserva reserva = new Reserva();
-            reserva.setEstudiante(estudiantes.get(random.nextInt(estudiantes.size())));
-            reserva.setSala(salas.get(random.nextInt(salas.size())));
-            reserva.setFechaSolicitada(new Date());
-            reserva.setHoraReserva(new Date());
-            reserva.setHoraCierre(new Date(System.currentTimeMillis() + faker.number().numberBetween(3600000, 7200000))); // 1-2 horas más
-            reserva.setEstado(faker.number().numberBetween(0, 2));
-            reservaRepository.save(reserva);
         }
 
 
     }
+
 }
--- src/main/java/biblioteca/salas/duoc/biblioteca/salas/duoc/repository/CarreraRepository.java
+++ src/main/java/biblioteca/salas/duoc/biblioteca/salas/duoc/repository/CarreraRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CarreraRepository extends JpaRepository<Carrera, String>{
+
 }
